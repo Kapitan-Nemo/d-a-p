54a418282d0c9e245b94cd633b50bf1705780e19
Kapitan Nemo <lasota020@gmail.com>
2023-05-16 18:19:19 +0200

    clip path shape

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="w-fulll h-20 bg-black my-6 ml-20">
+  <div class="w-fulll h-20 bg-black my-6 ml-20 shape">
     <h1 class="text-white text-4xl font-bold h-full flex justify-center items-center pl-5">
       "Pasterz mebli i sprzętów z żelaza, ubrany w prześcieradła"
     </h1>
   </div>
 </template>
+
+<style lang="scss">
+//sharped rectangle from left side
+.shape {
+    clip-path: polygon(5% 0, 100% 0%, 100% 100%, 0 100%);
+}
+</style>
