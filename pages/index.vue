1e1775ce02e9f451e292d45dee5898e3e05e042b
Kapitan Nemo <lasota020@gmail.com>
2023-05-16 18:13:08 +0200

    homepage black banner title

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="container">
-    This is INDEX.VUE
+  <div class="w-fulll h-20 bg-black my-6 ml-20">
+    <h1 class="text-white text-4xl font-bold h-full flex justify-center items-center pl-5">
+      "Pasterz mebli i sprzętów z żelaza, ubrany w prześcieradła"
+    </h1>
   </div>
 </template>
