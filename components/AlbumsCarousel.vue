68366eb1f3950754299c210107769942739c7557
Kapitan Nemo <lasota020@gmail.com>
2023-05-18 18:32:52 +0200

    releases module update

--- components/AlbumsCarousel.vue
+++ components/AlbumsCarousel.vue
@@ -4,28 +4,41 @@ const releases = ref([{
   title: 'NMS 026 Wędrowcy Tułacze Zbiegi "Berliner Vulkan" ltd. 12"MLP',
   description: 'Zabiorę Cię na wojnę z motylami.',
   image: 'berliner-vulkan.png',
+  quantity: 0,
 }, {
   id: 2,
   title: 'NMS 016 Wędrowcy Tułacze Zbiegi "Światu jest wszystko jedno" 12" ',
   description: '...lecz w brzuchu ziemi do końca wszechświata, żył będę z olbrzymami!',
   image: 'swiatu-jest-wszystko-jedno.png',
+  quantity: 100,
 }, {
   id: 3,
   title: 'NMS 030 Gruzja "Pierwszy koncert w mieście" ltd. 7"',
   description: 'Gruzja na Cykladach. Gruzja na własnych zasadach. Tylko czysty Grues.',
   image: 'pierwszy-koncert-w-miescie.png',
+  quantity: 2,
 }, {
   id: 4,
   title: 'NMS 000 DUSZĘ WYPUŚCIŁ "przekrólewszczenie zero" GLP+LP"',
   description: 'Będziemy widzieć takie rzeczy, w kótre i tak nikt nie uwierzy.',
   image: 'przekrolewszczenie-zero.png',
+  quantity: 9,
 },
 {
   id: 5,
+  title: 'NMS 036 ODRAZA "Rzeczom" tape ltd. 150',
+  description: 'Dziś staram się bardzo nie starać, Dziś staram się raczej nie dawać',
+  image: 'rzeczom.png',
+  quantity: 25,
+},
+{
+  id: 6,
   title: 'NMS 029 Koniec Pola "trop" DigiBook ltd. 100"',
   description: 'Zbieraj się, idziemy. Weź nasze. Zostaw swoje.',
   image: 'trop.png',
+  quantity: 55,
 },
+
 ])
 // TODO: move to composables when this bug will be fixed:
 // https://github.com/nuxt/nuxt/issues/20827
@@ -44,23 +57,27 @@ function useAssets(path: string): string {
     <h2 class="text-4xl font-bold mb-6">
       Releases
     </h2>
+
     <Swiper
-      :height="500"
-      :modules="[SwiperAutoplay, SwiperEffectCreative, SwiperNavigation]"
       :slides-per-view="5"
+      :space-between="50"
     >
+      <SwiperControls />
+
       <SwiperSlide v-for="(release, idx) in releases" :key="idx">
         <div>
-          <img class="mb-3" :src="useAssets(release.image)" :alt="release.title">
-          <p class="font-bold">
+          <span class="relative">
+            <p class="bg-white text-bold text-black py-3 px-5 absolute mt-2 ml-2" :class="release.quantity <= 10 ? 'text-red-700' : ' text-black' ">{{ release.quantity < 10 && release.quantity > 0 ? 'last copies' : release.quantity <= 0 ? 'sold out' : 'out now' }}</p>
+            <img class="mb-3" :src="useAssets(release.image)" :alt="release.title">
+          </span>
+          <p class="font-bold mb-1">
             {{ release.title }}
           </p>
-          <p>{{ release.description }}</p>
+          <p>
+            {{ release.description }}
+          </p>
         </div>
-
-      <!-- <p>{{ release.description }}</p> -->
       </SwiperSlide>
-      <SwiperControls />
     </Swiper>
   </div>
 </template>
--- components/SwiperControls.vue
+++ components/SwiperControls.vue
@@ -3,10 +3,12 @@ const swiper = useSwiper()
 </script>
 
 <template>
-  <button class="mr-6" @click="swiper.slidePrev()">
-    Previous
-  </button>
-  <button @click="swiper.slideNext()">
-    Next
-  </button>
+  <div class="flex justify-end mt-6">
+    <button class="mr-6 bg-black p-2" @click="swiper.slidePrev()">
+      <img src="@/assets/images/svg/arrow-left.svg" alt="arrow-left">
+    </button>
+    <button class="bg-black p-2" @click="swiper.slideNext()">
+      <img src="@/assets/images/svg/arrow-right.svg" alt="arrow-right">
+    </button>
+  </div>
 </template>
