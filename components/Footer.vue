d9b2f1bfafbef25ace7efb429dfc47148b110daa
Kapitan Nemo <lasota020@gmail.com>
2023-05-23 16:43:22 +0200

    footer center text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="mt-3">
     <div class="bg-black h-20 flex justify-center items-center">
-      <p class="px-3 text-xl lg:text-2xl text-white font-bold">
+      <p class="text-center px-3 text-xl lg:text-2xl text-white font-bold">
         As of the '20s, you can subscribe to the D-A-P mailing list - <a href="#" class="underline underline-offset-4">Here</a>.
       </p>
     </div>
