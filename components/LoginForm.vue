e63624f9122d5f953033307d96ddcac5acb2560d
Kapitan Nemo <lasota020@gmail.com>
2023-05-29 22:42:05 +0200

    ux/ui improve modal login component

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -32,13 +32,18 @@ onMounted(() => {
 </script>
 
 <template>
+  <!-- TODO: google icon  -->
   <button v-if="!auth.logged" class="px-8 py-2 text-3xl  bg-white border-4 border-black font-bold text-black rounded-none" @click="singInWithGoogle">
     Sign in with Google
   </button>
-  <div v-if="auth.logged">
-    <h2 class="text-2xl font-bold">
+  <div v-if="auth.logged" class="flex flex-col items-center ">
+    <h2 class="text-2xl font-bold mb-6">
       Hi, {{ auth.userName }} <span class="wave">👋</span>
     </h2>
+    <button class="bg-black px-8 py-2 text-3xl font-bold text-white rounded-none flex items-center justify-center">
+      Settings
+    </button>
+    <span class="my-3 font-bold text-2xl">or</span>
     <button class="px-8 py-2 text-3xl  bg-white border-4 border-black font-bold text-black rounded-none" @click="singOutGoogle">
       Log Out
     </button>
@@ -47,21 +52,21 @@ onMounted(() => {
 
 <style  lang="scss" scoped>
 .wave {
-  animation-name: wave-animation;  /* Refers to the name of your @keyframes element below */
-  animation-duration: 2.5s;        /* Change to speed up or slow down */
-  animation-iteration-count: infinite;  /* Never stop waving :) */
-  transform-origin: 70% 70%;       /* Pivot around the bottom-left palm */
+  animation-name: wave-animation;
+  animation-duration: 2.5s;
+  animation-iteration-count: infinite;
+  transform-origin: 70% 70%;
   display: inline-block;
 }
 
 @keyframes wave-animation {
     0% { transform: rotate( 0.0deg) }
-   10% { transform: rotate(14.0deg) }  /* The following five values can be played with to make the waving more or less extreme */
+   10% { transform: rotate(14.0deg) }
    20% { transform: rotate(-8.0deg) }
    30% { transform: rotate(14.0deg) }
    40% { transform: rotate(-4.0deg) }
    50% { transform: rotate(10.0deg) }
-   60% { transform: rotate( 0.0deg) }  /* Reset for the last half to pause */
+   60% { transform: rotate( 0.0deg) }
   100% { transform: rotate( 0.0deg) }
 }
 </style>
--- components/Modal.vue
+++ components/Modal.vue
@@ -2,15 +2,14 @@
 const modal = useModal()
 </script>
 
+<!-- TODO: close modal when click outside -->
 <template>
-  <div v-show="modal.show" class="absolute bg-white top-10 right-10 w-96 h-96 justify-between p-6 flex flex-col shadow">
+  <div v-show="modal.show" class="absolute bg-white top-10 right-10 w-96 h-96 justify-center items-center p-6 flex flex-col shadow">
+    <button class="absolute top-1 right-1 bg-black text-2xl font-bold w-8 h-8 flex items-center justify-center text-white" @click="modal.show = false">
+      x
+    </button>
     <div class="modal-body">
       <slot />
-    </div>
-    <div class="modal-footer">
-      <button @click="modal.show = false">
-        Close
-      </button>
     </div>
   </div>
 </template>
