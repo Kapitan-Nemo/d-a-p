a1b4dfcc7da073091402f49292c5cc14884c01b6
Kapitan Nemo <lasota020@gmail.com>
2023-05-30 23:49:24 +0200

    sign in / log in functions

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
 
 const auth = useAuth()
+const email = ref('')
+const password = ref('')
 
 function singInWithGoogle() {
   const provider = new GoogleAuthProvider()
@@ -12,6 +14,37 @@ function singInWithGoogle() {
     })
     .catch((error) => {
       console.log(error)
+    })
+}
+
+function userCreateAccount() {
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((userCredential) => {
+    // Signed in
+      const user = userCredential.user
+      console.log(user)
+      console.log('success')
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      console.log(errorCode)
+      const errorMessage = error.message
+      console.log(errorMessage)
+    })
+}
+
+function singInWithEmailAndPassword() {
+  signInWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((userCredential) => {
+    // Signed in
+      const user = userCredential.user
+      console.log(user)
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      console.log(errorCode)
+      const errorMessage = error.message
+      console.log(errorMessage)
     })
 }
 
@@ -33,16 +66,16 @@ onMounted(() => {
 
 <template>
   <!-- TODO: google icon  -->
-  <div v-if="!auth.logged" class="flex items-center flex-col max-h-96">
+  <div v-if="!auth.logged" class="flex items-center flex-col">
     <button class="px-8 py-2 text-3xl  bg-white border-4 border-black font-bold text-black rounded-none" @click="singInWithGoogle">
       Sign in with Google
     </button>
-    <span class="my-3 font-bold text-2xl">OR</span>
-    <button class="px-8 py-2 text-3xl mb-6  bg-white border-4 border-black font-bold text-black rounded-none">
+    <span class="my-6 font-bold text-2xl">OR</span>
+    <input v-model="email" class="border-b border-black border-spacing-y-4 h-10 text-black caret-black placeholder-gray-500 text-2xl mb-3 focus:outline-none" type="text" placeholder="email">
+    <input v-model="password" class="border-b border-black border-spacing-y-4 h-10 text-black caret-black placeholder-gray-500 text-2xl mb-6 focus:outline-none" type="password" placeholder="password">
+    <button class="px-8 py-2 text-3xl mb-6  bg-black border-4 border-black font-bold text-white" @click="userCreateAccount">
       Email sign in
     </button>
-    <input class="border-b border-black border-spacing-y-4 h-10 text-black caret-black placeholder-gray-500 text-2xl mb-3 focus:outline-none" type="text" placeholder="email">
-    <input class="border-b border-black border-spacing-y-4 h-10 text-black caret-black placeholder-gray-500 text-2xl focus:outline-none" type="password" placeholder="password">
   </div>
   <div v-if="auth.logged" class="flex flex-col items-center ">
     <h2 class="text-2xl font-bold mb-6">
