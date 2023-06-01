1f9169d11016d13e3c00494de6190c608334b41a
Kapitan Nemo <kapitan_nemo@MacBook-Pro-Kapitan.local>
2023-06-01 22:24:27 +0200

    toast id

--- composables/useToast.ts
+++ composables/useToast.ts
@@ -1,5 +1,7 @@
 export default function useToast(message: string, type: string, duration: number) {
   const toast = document.createElement('div')
+  const id = document.querySelectorAll('.toast').length
+  toast.id = `toast-${id}`
   toast.classList.add('toast')
   toast.classList.add(`toast--${type}`)
   toast.textContent = message
