e281433301604b4e66ddc26762b491ebe495895e
Kapitan Nemo <lasota020@gmail.com>
2023-06-03 11:14:26 +0200

    add progress bar

--- composables/useToast.ts
+++ composables/useToast.ts
@@ -4,9 +4,23 @@ export default function useToast(message: string, type: string, duration: number
   toast.style.bottom = `${position}px`
   toast.classList.add('toast')
   toast.classList.add(`toast--${type}`)
-  toast.textContent = message
+  toast.innerHTML = `${message}<span style="width:0%" class="toast--progress"></span>`
 
   document.body.appendChild(toast)
+
+  const elem = document.querySelector('.toast--progress') as HTMLElement
+
+  let width = 0
+  const bar = setInterval(frame, duration / 100)
+  function frame() {
+    if (width >= 100) {
+      clearInterval(bar)
+    }
+    else {
+      width++
+      elem.style.width = `${width}%`
+    }
+  }
 
   setTimeout(() => {
     toast.remove()
