0fe0ffcea466e929fa0a6df92b7b6960add85806
Kapitan Nemo <lasota020@gmail.com>
2023-06-03 10:39:51 +0200

    remove middleware / if for not logged in

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
 const auth = useAuth()
-
-definePageMeta({
-  middleware() {
-    const auth = useAuth()
-    if (!auth.logged)
-      return navigateTo('/')
-  },
-})
 </script>
 
 <template>
@@ -15,7 +7,7 @@ definePageMeta({
     Settings
   </Header>
   <div class="default-height px-6">
-    <ClientOnly>
+    <ClientOnly v-if="auth.logged">
       <p>User name: {{ auth.userName }}</p>
       <p>User Last name: {{ auth.userLastName }}</p>
       <p>User street: {{ auth.userStreet }}</p>
@@ -25,5 +17,8 @@ definePageMeta({
       <p>User email: {{ auth.userEmail }}</p>
       <p>User id: {{ auth.userId }}</p>
     </ClientOnly>
+    <h2 v-else class="flex items-center justify-center default-height text-2xl font-bold text-center">
+      You are not sign in
+    </h2>
   </div>
 </template>
