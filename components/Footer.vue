54a36e3d11b8b5a48361a07b089e63a6fa299039
Kapitan Nemo <lasota020@gmail.com>
2023-06-08 00:33:07 +0200

    footer font mobile

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="mt-3">
     <div class="bg-black h-20 flex justify-center items-center">
-      <p class="text-center px-3 text-xl lg:text-2xl text-white font-bold">
+      <p class="text-center px-3 text-sm md:text-2xl text-white font-bold">
         As of the '20s, you can subscribe to the D-A-P mailing list - <span class="underline underline-offset-4">Here</span>.
       </p>
     </div>
