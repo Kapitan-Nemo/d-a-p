5ca30abbdccf1b1bc5b6ba3c2ba7bbbe93e9d432
Kapitan Nemo <lasota020@gmail.com>
2023-06-08 18:50:36 +0200

    admin dashboard layout

---
+++ components/admin/AdminAside.vue
@@ -0,0 +1,23 @@
+<template>
+  <aside id="sidebar" class="fixed top-0 left-0 z-20 flex flex-col w-64 h-full pt-18 lg:flex" aria-label="Sidebar">
+    <div class="flex flex-col pt-0 bg-black h-full">
+      <ul class="p-3">
+        <li class="mb-3">
+          <nuxt-link to="/admin" class="flex items-center p-2 text-white">
+            <svg class="w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" /><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" /></svg>
+            <span class="ml-3">Dashboard</span>
+          </nuxt-link>
+        </li>
+
+        <li class="mb-3">
+          <button type="button" class="flex items-center w-full p-2 text-white">
+            <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path clip-rule="evenodd" fill-rule="evenodd" d="M.99 5.24A2.25 2.25 0 013.25 3h13.5A2.25 2.25 0 0119 5.25l.01 9.5A2.25 2.25 0 0116.76 17H3.26A2.267 2.267 0 011 14.74l-.01-9.5zm8.26 9.52v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.615c0 .414.336.75.75.75h5.373a.75.75 0 00.627-.74zm1.5 0a.75.75 0 00.627.74h5.373a.75.75 0 00.75-.75v-.615a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625zm6.75-3.63v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75zM17.5 7.5v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75z" />
+            </svg>
+            <span class="ml-3">Products</span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </aside>
+</template>
--- pages/admin.vue
+++ pages/admin.vue
@@ -8,6 +8,7 @@ definePageMeta({
   <div>
     <NuxtLayout name="admin">
       <AdminNav />
+      <AdminAside />
     </NuxtLayout>
   </div>
 </template>
