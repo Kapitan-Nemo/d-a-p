4395325263a905c008d592ede050a40034340990
Kapitan Nemo <lasota020@gmail.com>
2023-06-10 00:43:51 +0200

    handle select of checkbox

--- pages/admin/products.vue
+++ pages/admin/products.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
+import type IAlbum from '~/components/constants/interface'
+
 definePageMeta({
   middleware: [
     'admin',
   ],
 })
 const { data: albums } = await useFetch('/api/albums')
-const selectedProducts = ref([])
-// const checkedProducts = ref(false)
+const selectedProducts = ref([] as IAlbum[])
+const selectedAll = ref(false)
+
+function selectAll() {
+  selectedAll.value ? selectedProducts.value = [] : selectedProducts.value = albums.value as IAlbum[]
+  selectedAll.value = !selectedAll.value
+}
 </script>
 
 <template>
@@ -16,7 +23,7 @@ const selectedProducts = ref([])
         <tr>
           <th scope="col" class="p-4">
             <div class="flex items-center">
-              <input id="checkbox-all" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" @click="selectedProducts = []">
+              <input id="checkbox-all" v-model="selectedAll" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2 " @click="selectAll">
             </div>
           </th>
           <th scope="col" class="px-6 py-3">
