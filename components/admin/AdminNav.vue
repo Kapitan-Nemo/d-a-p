2204c3656c546068a9f6bf908eaf1065824c7ac0
Kapitan Nemo <lasota020@gmail.com>
2023-06-13 21:02:58 +0200

    icons for actions in admin panel

--- components/admin/AdminNav.vue
+++ components/admin/AdminNav.vue
@@ -12,7 +12,7 @@ const auth = useAuth()
           </div>
         </div>
         <div class="flex items-center">
-          <button type="button" class="p-2 text-red-400 rounded-lg">
+          <button type="button" class="p-2">
             <svg class="w-6 h-6 text-red-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" /></svg>
           </button>
 
