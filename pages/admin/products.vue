de47ac57edcd36cdc3e8f934fe460b69735a4929
Kapitan Nemo <lasota020@gmail.com>
2023-06-16 23:29:06 +0200

    drawer styling improve

--- pages/admin/products.vue
+++ pages/admin/products.vue
@@ -116,22 +116,22 @@ function editProduct(product: IAlbum) {
     </table>
   </div>
   <!-- drawer component -->
-  <div id="drawer-example" :class="editMode ? 'transform-none' : '-translate-x-full'" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-dark-200" tabindex="-1" aria-labelledby="drawer-label">
-    <h5 id="drawer-label" class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">
+  <div :class="editMode ? 'transform-none' : '-translate-x-full'" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-dark-200">
+    <h5 class="inline-flex items-center mb-4 text-base font-semibold text-white">
       <svg class="w-5 h-5 mr-2" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" /></svg>Info
     </h5>
-    <button type="button" data-drawer-hide="drawer-example" aria-controls="drawer-example" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" @click="editMode = !editMode">
+    <button type="button" class="text-white hover:bg-dark-100 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center" @click="editMode = !editMode">
       <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
     </button>
     <div class="flex flex-col">
-      <input v-model="currentEdit.title" class="mb-3 w-full border-b border-white bg-dark-200 h-8 text-white caret-white placeholder-gray-500 focus:outline-none" type="text">
-      <textarea v-model="currentEdit.description" class="mb-3 w-full border-b border-white bg-dark-200 h-8 text-white caret-white placeholder-gray-500 focus:outline-none" type="textarea" />
+      <textarea v-model="currentEdit.title" rows="3" cols="40" class="mb-3 w-full border-b border-white bg-dark-200 text-white caret-white placeholder-gray-500 focus:outline-none" type="textarea" />
+      <textarea v-model="currentEdit.description" rows="3" cols="40" class="mb-3 w-full border-b border-white bg-dark-200 text-white caret-white placeholder-gray-500 focus:outline-none" type="textarea" />
       <input v-model="currentEdit.price" class="mb-3 w-full border-b border-white bg-dark-200 h-8 text-white caret-white placeholder-gray-500 focus:outline-none" type="text">
       <input v-model="currentEdit.quantityInWarehouse" class="mb-3 w-full border-b border-white bg-dark-200 h-8 text-white caret-white placeholder-gray-500 focus:outline-none" type="text">
       <input v-model="currentEdit.featured" type="checkbox" class="mb-3 w-full border-b border-white bg-dark-200 h-8 text-white caret-white placeholder-gray-500 focus:outline-none">
+      <!-- TODO:  image uploader -->
       <img :src="`/images/${currentEdit.image}`" alt="product image">
-      <!-- TODO: image uploader -->
-      <button class="mt-3 bg-red-300 hover:bg-red-600 text-black font-bold py-2 px-4 rounded">
+      <button class="mt-3 bg-black text-white font-bold py-2 px-4 rounded">
         Update
       </button>
     </div>
