89a9fd266f946aede869196f564f2ad1be3098db
Kapitan Nemo <lasota020@gmail.com>
2023-06-22 20:17:21 +0200

    package update / middleware use toast

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -1,6 +1,8 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   const auth = useAuth()
   const config = useRuntimeConfig()
-  if (!auth.logged || config.public.FIREBASE_ADMINID !== auth.userId)
+  if (!auth.logged || config.public.FIREBASE_ADMINID !== auth.userId) {
+    await useToast('You are not authorized to access this page', 'error')
     return navigateTo('/')
+  }
 })
