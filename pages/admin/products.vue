e3142c78167127f9e938d7794cf823837eb95f12
Kapitan Nemo <lasota020@gmail.com>
2023-06-23 14:01:55 +0200

    fix create / update state of product

--- pages/admin/products.vue
+++ pages/admin/products.vue
@@ -61,6 +61,8 @@ watch(() => show.value.edit, () => {
 function editProducts(product: IAlbum) {
   editProduct.value = product
   show.value.edit = !show.value.edit
+  show.value.create = false
+  console.log(show.value.create)
 }
 
 async function deleteProduct(id: string) {
@@ -87,8 +89,9 @@ function createProduct() {
     featured: false,
     link: '',
   }
-  show.value.create = true
   show.value.edit = !show.value.edit
+  show.value.create = true
+  console.log(show.value.create)
 }
 
 async function saveProduct(id: string) {
@@ -195,7 +198,7 @@ async function saveProduct(id: string) {
   <div :class="show.edit ? 'transform-none' : '-translate-x-full'" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-dark-200">
     <h5 class="inline-flex items-center mb-4 text-base font-semibold text-white">
       <svg class="w-5 h-5 mr-2" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" /></svg>
-      {{ show.create ? 'Create' : 'Update' }} product
+      {{ !show.create ? 'Update' : 'Create' }} product
     </h5>
     <button type="button" class="text-white hover:bg-dark-100 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center" @click="(show.edit = !show.edit) && (show.create = !show.create)">
       <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
