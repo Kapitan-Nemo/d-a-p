13d823dad919dfd24e7d4ffb4495038c27a577eb
Kapitan Nemo <lasota020@gmail.com>
2023-06-27 15:38:47 +0200

    userStore for checkout form

--- components/cart/CartSummary.vue
+++ components/cart/CartSummary.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 
+// import { collection, doc, getFirestore, setDoc } from 'firebase/firestore'
+
 defineProps({
   link: {
     string: true,
@@ -8,11 +10,55 @@ defineProps({
   },
 })
 
+const userStore = useAuth()
 const cartStore = useCart()
 const { cart } = storeToRefs(cartStore)
+const { userName, userLastName, userStreet, userCity, userZipCode, userPhone, userId, userEmail } = storeToRefs(userStore)
 const cartTotalPrice = computed(() => {
   return cart.value.reduce((acc, product) => acc + product.price * product.quantityInCart, 0)
 })
+
+function payment() {
+  console.log('payment')
+  console.log(userName.value)
+  console.log(userLastName.value)
+  console.log(userStreet.value)
+  console.log(userCity.value)
+  console.log(userZipCode.value)
+  console.log(userPhone.value)
+  console.log(userId.value)
+  console.log(userEmail.value)
+}
+// const config = useRuntimeConfig()
+// const stripe = await loadStripe(config.public.STRIPE_PK)
+
+// function order() {
+//   const orders = doc(collection(getFirestore(), 'orders'))
+//   const data = {
+//     ...cartStore.cart,
+//   }
+//   // set doc with id
+//   setDoc(orders, data)
+//     .then(() => {
+//       useToast('Order created successfully', 'success')
+//     })
+//     .catch((error) => {
+//       console.error('Error adding document: ', error)
+//       useToast('Error adding document', 'error')
+//     })
+
+//   // const stripeValues = cart.value.map(a => ({
+//   //   price: a.stripeId,
+//   //   quantity: a.quantityInCart,
+//   // }))
+
+//   // stripeValues?.redirectToCheckout({
+//   //   lineItems,
+//   //   mode: 'payment',
+//   //   successUrl: `https://${window.location.host}/success`,
+//   //   cancelUrl: `https://${window.location.host}/`,
+//   // })
+// }
 </script>
 
 <template>
@@ -21,15 +67,24 @@ const cartTotalPrice = computed(() => {
       Your Order
     </h2>
     <div>
-      <p>Number of products: {{ cartStore.cartTotalProducts }}</p>
-      <p>Total price: {{ cartTotalPrice }}</p>
-      <p>Delivery: Whoop! Standard shipping is free</p>
+      <p>Number of products: <span class="font-bold">{{ cartStore.cartTotalProducts }}</span></p>
+      <p>Total price: <span class="font-bold">{{ cartTotalPrice }}</span></p>
+      <p>Delivery: Whoop! Standard shipping is <span class="font-bold">free</span></p>
     </div>
 
-    <div class="flex">
-      <NuxtLink class="mt-6 w-full px-8 py-2 bg-black border-black font-bold text-white" :to="`${link}`">
-        {{ link as string === 'payment' ? `Pay ${cartTotalPrice} $` : 'Check Out' }}
+    <div class="flex flex-col">
+      <NuxtLink v-if="link === 'checkout'" class="mt-6 w-full px-8 py-2 bg-black border-black font-bold text-white" :to="`${link}`">
+        Check Out ->
       </NuxtLink>
+      <div v-if="link === 'payment'" class="flex flex-col">
+        <button class="mt-6 w-full px-8 py-2 bg-black border-black font-bold text-white" @click="payment">
+          Pay {{ cartTotalPrice }} $
+        </button>
+        <small class="font-bold text-center my-2">OR</small>
+        <NuxtLink v-if="link === 'payment'" class="w-full font-bold under text-black text-center underline underline-offset-8" to="/cart">
+          Back to Cart
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,41 +1,9 @@
 <script setup lang="ts">
-// import { collection, doc, getFirestore, setDoc } from 'firebase/firestore'
-const user = ref({
-  name: '',
-  lastName: '',
-  email: '',
-})
+import { storeToRefs } from 'pinia'
 
-// const config = useRuntimeConfig()
-// const stripe = await loadStripe(config.public.STRIPE_PK)
+const userStore = useAuth()
 
-// function order() {
-//   const orders = doc(collection(getFirestore(), 'orders'))
-//   const data = {
-//     ...cartStore.cart,
-//   }
-//   // set doc with id
-//   setDoc(orders, data)
-//     .then(() => {
-//       useToast('Order created successfully', 'success')
-//     })
-//     .catch((error) => {
-//       console.error('Error adding document: ', error)
-//       useToast('Error adding document', 'error')
-//     })
-
-//   // const stripeValues = cart.value.map(a => ({
-//   //   price: a.stripeId,
-//   //   quantity: a.quantityInCart,
-//   // }))
-
-//   // stripeValues?.redirectToCheckout({
-//   //   lineItems,
-//   //   mode: 'payment',
-//   //   successUrl: `https://${window.location.host}/success`,
-//   //   cancelUrl: `https://${window.location.host}/`,
-//   // })
-// }
+const { userName, userLastName, userStreet, userCity, userZipCode, userPhone, userEmail } = storeToRefs(userStore)
 </script>
 
 <template>
@@ -55,25 +23,25 @@ const user = ref({
           <span>1. Address </span>
         </h2>
         <div class="flex flex-col w-1/2">
-          <div class="">
-            <input v-model="user.name" required autocomplete="name" class="mr-3 border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" name="name" type="text" placeholder="Name">
-            <input v-model="user.lastName" required autocomplete="lastName" class="border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" name="lastName" type="text" placeholder="Last name">
+          <div>
+            <input v-model="userName" required autocomplete="name" class="mr-3 border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" name="name" type="text" placeholder="Name">
+            <input v-model="userLastName" required autocomplete="lastName" class="border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" name="lastName" type="text" placeholder="Last name">
           </div>
           <div>
-            <input required autocomplete="city" class="mr-3 border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="city" placeholder="City">
-            <input required autocomplete="zip" class="border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="zip" placeholder="Zip">
+            <input v-model="userCity" required autocomplete="city" class="mr-3 border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="city" placeholder="City">
+            <input v-model="userZipCode" required autocomplete="zip" class="border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="zip" placeholder="Zip">
           </div>
           <div>
-            <input v-model="user.email" required autocomplete="email" class="mr-3 border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="email" placeholder="Email">
-            <input required autocomplete="phone" class="border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="phone" placeholder="Phone">
+            <input v-model="userEmail" required autocomplete="email" class="mr-3 border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="email" placeholder="Email">
+            <input v-model="userPhone" required autocomplete="phone" class="border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="phone" placeholder="Phone">
           </div>
 
-          <input required autocomplete="address" class="border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="address" placeholder="Address">
+          <input v-model="userStreet" required autocomplete="address" class="border-b border-black h-8 text-black caret-black placeholder-gray-500 mb-3 focus:outline-none" type="text" name="address" placeholder="Address">
         </div>
         <h2 class="text-2xl font-bold mb-3">
           2. Delivery method
         </h2>
-        <div class="flex flex-col w-1/2">
+        <div class="flex flex-col w-full">
           <div class="flex items-center mb-3">
             <input id="delivery" checked type="radio" class="mr-3 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" name="delivery">
             <label for="delivery">DPD Delivery</label>
